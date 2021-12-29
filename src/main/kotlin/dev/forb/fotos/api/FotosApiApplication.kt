adabb360e960b94bb80f4f00ebdf6b5b08e92ccc
Forbidden-A <Forbidden.BA@ProtonMail.com>
2021-12-29 19:48:37 +0300

    Created spring project

---
+++ src/main/kotlin/dev/forb/fotos/api/FotosApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.forb.fotos.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FotosApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FotosApiApplication>(*args)
+}
